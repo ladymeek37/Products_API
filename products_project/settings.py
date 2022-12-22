554a133e4630b5b99c5166fdd4cf804fc12456cb
Lady Meek <ladyjmeek@gmail.com>
2022-12-22 15:45:58 -0600

    Updated settings errors + created products app

--- products_project/settings.py
+++ products_project/settings.py
@@ -35,8 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
-    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -47,7 +45,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
